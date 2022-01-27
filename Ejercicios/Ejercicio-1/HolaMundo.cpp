c274fda4d850c93ab5b034ac001d915ebd2e3181
Jose Dubon <josedubon17@hotmail.com>
2022-01-26 23:15:23 -0600

    Creacion de 10 Carpetas Video 11

---
+++ Ejercicios/Ejercicio-1/HolaMundo.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "Hola Mundo :D";
+
+    return 0;
+}
---
+++ Ejercicios/Ejercicio-2/mensajes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    
+    string nombre = "";
+
+    cout << "Ingrese su nombre :";
+    cin >> nombre;
+
+    cout<< endl;
+    cout<< "Mi nombre es: " << nombre;
+    cout<< endl;
+
+    return 0;
+}
