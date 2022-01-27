6a9fd50074ccec0628249d50c84c04f3fbb759a3
Jose Dubon <josedubon17@hotmail.com>
2022-01-26 23:46:39 -0600

    Video 14 Tipos de Datos

---
+++ Ejercicios/Ejercicio3 Variables/variables.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    //////Variables: nos sirven para almacenar datos en memoria, los datos tienen tipos
+    /////entonces hay tipos de variables.
+    int numeroEntero;
+    numeroEntero = 15;
+
+    cout << "Entero: " << numeroEntero << endl;
+
+    numeroEntero = 18;
+
+    cout << "Entero modificado: " << numeroEntero;
+
+    return 0;
+}
