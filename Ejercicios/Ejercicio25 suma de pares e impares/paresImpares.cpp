ee51b61b28db89a7e4cda2b73ec9eb1f26e88fcc
Jose Dubon <josedubon17@hotmail.com>
2022-02-16 21:25:06 -0600

    Tarea 4 Pares, Impares Total

---
+++ Ejercicios/Ejercicio25 suma de pares e impares/paresImpares.cpp
@@ -0,0 +1,31 @@
+#include <iostream> 
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int pares = 0, impares = 0;
+
+    for (int i = 1; i <= 10; i++)
+    {
+        if (i % 2 == 0)
+        {
+            pares = pares + i;
+        }
+        else 
+        {
+            cout << endl;
+            impares = impares +i;
+        }
+        cout << i << " ";
+    }
+    
+    cout << endl;
+    cout << endl;
+    cout << "Total pares: " << pares;
+    cout << endl;
+    cout << "Total impares: " << impares;
+
+
+    return 0;
+}
