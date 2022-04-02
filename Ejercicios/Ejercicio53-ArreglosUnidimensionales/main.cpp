1d20e7cd652af144c9236197a7b4eaf5b6052d17
Jose Dubon <josedubon17@hotmail.com>
2022-04-01 20:54:42 -0600

    Arreglos Parte I

---
+++ Ejercicios/Ejercicio53-ArreglosUnidimensionales/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+ using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int numeros[5]; //declaracion del arreglo
+
+
+    //asignacion de valores a un arreglo unidimensional
+    numeros[0] = 10;
+    numeros[1] = 5;
+    numeros[2] = 7;
+    numeros[3] = 50;
+    numeros[4] = 2;
+    
+    /// lectura de un arreglo unidimencional
+    cout << numeros[0] << endl;
+    cout << numeros[1] << endl;
+    cout << numeros[2] << endl;
+    cout << numeros[3] << endl;
+    cout << numeros[4] << endl;
+
+    return 0;
+}
+
+ 
